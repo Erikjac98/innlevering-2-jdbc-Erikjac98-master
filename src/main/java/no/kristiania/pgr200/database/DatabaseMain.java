bd84c2133f80bb5e371879bb3801ae42ad12a480
Erikjac98 <jaceri17@student.westerdals.no>
2018-10-31 15:45:03 +0100

    Fjernet unødvendige klasser

--- src/main/java/no/kristiania/pgr200/database/DatabaseMain.java
+++ src/main/java/no/kristiania/pgr200/database/DatabaseMain.java
@@ -5,19 +5,27 @@ import java.io.IOException;
 import java.sql.*;
 
 public class DatabaseMain {
-    public static void createNewTable() {
+    public static void createTable() {
 
         String url = "jdbc:postgresql://localhost:5432/postgres";
 
-        String sql = "CREATE TABLE Conference (\n" + "conference_id integer PRIMARY KEY, \n" + "days integer NOT NULL, \n"
+        String sql = "CREATE TABLE Conference (\n" + "conference_id integer PRIMARY KEY, \n" +
+                     "days integer NOT NULL, \n" + "date String NOT NULL \n" + "talk_id integer FOREIGN KEY\n" +
+                     ");";
+        try(Connection conn = DriverManager.getConnection(url);
+                Statement statement = conn.createStatement()){
+            statement.execute(sql);
+        }   catch(SQLException e) {
+            System.out.println(e.getMessage());
+        }
     }
 
 
 
 
 
-    public static void main(String[] args) throws IOException , SQLException {
-
+    public static void main(String[] args){
+        createTable();
 
     }
 
